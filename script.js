91eb195035d57d5053fc1f6d2689ed7663fcf332
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2022-09-26 18:31:45 -0500

    Added local storage functionality
    Added local storage functionality

--- script.js
+++ script.js
@@ -9,18 +9,27 @@ var input = document.createElement("input");
     document.body.appendChild(input);
 
 
+$(document).ready(function () {
+    for (var i = 9; i <= 17; i++) {
+        $("#" + i).text(localStorage.getItem(i))
+    }
+})
+
 
 function addClass () {
-	var hour = moment().format("H");
+	var hour = parseInt(moment().format("H"));
+    console.log(hour)
     var timeArray = document.querySelectorAll("textarea");
     timeArray.forEach(timeBlock => {
-    if (timeBlock.id === hour) {
+        var timeBlockId = parseInt(timeBlock.id)
+    if (timeBlockId === hour) {
     timeBlock.classList.add("present")
     }
-    if (timeBlock.id < hour) {
+    if (timeBlockId < hour) {
     timeBlock.classList.add("past")
     }
-    if (timeBlock.id > hour) {
+    if (timeBlockId > hour) {
+       console.log(typeof timeBlock.id,typeof hour)
         timeBlock.classList.add("future")
     }
 })
@@ -32,10 +41,7 @@ function saveEvent () {
    var timeBlockID = siblings[1].getAttribute("id");
    localStorage.setItem(timeBlockID, text);
 
-   for (var i=0; i < localStorage.timeBlockID.length; i++) {
-    var key = localStorage.timeBlockID(i);
-    var value = localStorage.getItem(key);
-   }
+   console.log(timeBlockID)   
 }
 
 $(".saveBtn").on("click", saveEvent)
