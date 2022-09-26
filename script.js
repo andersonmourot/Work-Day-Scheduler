db5ae4f1f1a02f2b4438bd196dc4d4b43a120d8b
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2022-09-26 16:58:54 -0500

    Added new functions and HTML
    Added new functions and HTML

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
+
+//Tracks and writes the current date
 var time = moment().format("dddd, MMMM Do");
 $("#currentDay").text(time);
 
-var hour = moment().format("h");
 
+var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+
+
+
+function addClass () {
+	var hour = moment().format("H");
+    var timeArray = document.querySelectorAll("textarea");
+    timeArray.forEach(timeBlock => {
+    if (timeBlock.id === hour) {
+    timeBlock.classList.add("present")
+    }
+    if (timeBlock.id < hour) {
+    timeBlock.classList.add("past")
+    }
+    if (timeBlock.id > hour) {
+        timeBlock.classList.add("future")
+    }
+})
+ }
+
+function saveEvent () {
+   var siblings = $(this).siblings();
+   var text = siblings[1].value;
+   var timeBlockID = siblings[1].getAttribute("id");
+   localStorage.setItem(timeBlockID, text);
+
+   for (var i=0; i < localStorage.timeBlockID.length; i++) {
+    var key = localStorage.timeBlockID(i);
+    var value = localStorage.getItem(key);
+   }
+}
+
+$(".saveBtn").on("click", saveEvent)
+
+addClass ();
+
+//console.log($(this).siblings("textarea")[0].value)
